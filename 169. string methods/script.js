2835b5530e2db6293faa1b069c3947fa7d098a88
sangamesh <smhadapad97@gmail.com>
2025-12-12 20:20:18 +0530

    string methods

---
+++ 169. string methods/script.js
@@ -0,0 +1,4 @@
+// const course = 'JavaScript 101: coding for beginners';
+course = 'JavaScript 101: coding for beginners';
+let codingNumber = course.search('coding');
+console.log(codingNumber);
---
+++ 170. Demystifying data types/script.js
@@ -0,0 +1,9 @@
+let list = ['one', 'two', 'three', 'four'];
+typeof [list];
+//  o/p - ('object');
+
+typeof list;
+// o/p - ('object');
+
+Array.isArray(list);
+// o/p - true;
