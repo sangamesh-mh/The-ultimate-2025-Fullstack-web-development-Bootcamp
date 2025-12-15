6671161065798d40abc8b6d4d2e68380193a4755
sangamesh <smhadapad97@gmail.com>
2025-12-15 22:08:56 +0530

    little changes to 174

--- 174. JavaScript Arrays/script.js
+++ 174. JavaScript Arrays/script.js
@@ -1,16 +1,21 @@
-lst = ["one", 2, 3.0, "four", [], {}];
-(6) ['one', 2, 3, 'four', Array(0), {…}]0: "one"1: 22: 33: "four"4: Array(0)length: 0[[Prototype]]: Array(0)5: [[Prototype]]: Objectlength: 6[[Prototype]]: Array(0)
-typeof(lst)
-'object'
-const lst = ["one", 2, 3.0, "four", [], {}];
-undefined
-lst.push("new item here");
-7
-lst
-(7) ['one', 2, 3, 'four', Array(0), {…}, 'new item here']0: "one"1: 22: 33: "four"4: []5: {}6: "new item here"length: 7[[Prototype]]: Array(0)
+lst = ['one', 2, 3.0, 'four', [], {}];
+
+typeof lst;
+// o/p - 'object'
+
+const lst = ['one', 2, 3.0, 'four', [], {}];
+
+lst.push('new item here');
+
+lst;
+// o/p - ['one', 2, 3, 'four', Array(0), {…}, 'new item here']0:
+
 const newItem = lst.pop();
-undefined
-newItem
-'new item here'
-lst
-(6) ['one', 2, 3, 'four', Array(0), {…}]0: "one"1: 22: 33: "four"4: []5: {}length: 6[[Prototype]]: Array(0)
+
+newItem;
+// o/p - 'new item here'
+
+lst;
+
+// o/p-
+// (6) ['one', 2, 3, 'four', Array(0), {…}]0: "one"1: 22: 33: "four"4: []5: {}length: 6[[Prototype]]: Array(0)
