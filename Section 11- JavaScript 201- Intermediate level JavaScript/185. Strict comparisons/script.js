371b87cbe37dcc8ac1b037680b2dea5aca7cc7c8
sangamesh <smhadapad97@gmail.com>
2026-01-17 19:37:45 +0530

    185. Strict comparisons

---
+++ Section 11- JavaScript 201- Intermediate level JavaScript/185. Strict comparisons/script.js
@@ -0,0 +1,23 @@
+let num = '0';
+
+console.log(num);
+// 0
+console.log(typeof num);
+
+// string
+
+console.log(num == 0);
+
+//  true
+
+console.log(num == '0');
+
+//  true
+
+console.log(num === 0);
+
+//  false
+
+console.log(num === '0');
+
+// true
